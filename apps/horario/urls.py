01da587f3bb3f471d74ca13bae9d394d7bc7ce62
brito-response <clodoaldo.brtp4@gmail.com>
2026-03-27 09:41:41 -0300

    transforming into cbv

--- apps/horario/urls.py
+++ apps/horario/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import horario_list, horario_create, horario_detail, horario_delete
+from .views import (HorarioListView,HorarioCreateView,HorarioDeleteView,HorarioDetailView)
 
 urlpatterns = [
-    path("", horario_list, name="horarios"),
-    path("create/", horario_create, name="create_horario"),
-    path("<int:id>/", horario_detail, name="horario_detail"),
-    path("<int:id>/delete/", horario_delete, name="horario_delete"),
+    path("", HorarioListView.as_view(), name="horario_list"),
+    path("create/", HorarioCreateView.as_view(), name="horario_create"),
+    path("<int:id>/delete/", HorarioDeleteView.as_view(), name="horario_delete"),
+    path("<int:id>/", HorarioDetailView.as_view(), name="horario_detail"),
 ]
--- apps/horario/views.py
+++ apps/horario/views.py
@@ -1,48 +1,54 @@
-from django.shortcuts import get_object_or_404, render, redirect
+from django.views.generic import ListView, CreateView, DeleteView, DetailView
+from django.shortcuts import get_object_or_404, redirect
+from django.urls import reverse_lazy
+from django.utils.decorators import method_decorator
+
 from apps.horario.forms import HorarioForm
 from core.decorators import acesso_required
 from .models import Horario
 from apps.agendamento.models import Agendamento
 
+class HorarioListView(ListView):
+    model = Horario
+    template_name = "horario/list.html"
+    context_object_name = "horarios"
+    
+@method_decorator(acesso_required(tipos=["MONITOR"], permissoes=["users.view_user"]),name="dispatch")
+class HorarioCreateView(CreateView):
+    model = Horario
+    form_class = HorarioForm
+    template_name = "horario/create.html"
+    success_url = "/horarios/"
 
-# Create your views here.
-def horario_list(request):
-    horarios = Horario.objects.all()
-    return render(request, "horario/list.html", {"horarios": horarios})
+    def form_valid(self, form):
+        horario = form.save(commit=False)
+        horario.monitor = self.request.user
+        horario.save()
+        return redirect(self.success_url)
+    
 
+@method_decorator(acesso_required(tipos=["MONITOR", "ADMIN"], permissoes=["users.view_user"]),name="dispatch")
+class HorarioDeleteView(DeleteView):
+    model = Horario
+    success_url = "/horarios/"
+    pk_url_kwarg = "id"
 
-@acesso_required(tipos=["MONITOR"], permissoes=["users.view_user"])
-def horario_create(request):
-    if request.method == "POST":
-        form = HorarioForm(request.POST)
-        if form.is_valid():
-            horario = form.save(commit=False)
-            horario.monitor = request.user
-            horario.save()
+    def dispatch(self, request, *args, **kwargs):
+        self.object = self.get_object()
+
+        if request.user != self.object.monitor and request.user.tipo != "ADMIN":
             return redirect("/horarios/")
-    else:
-        form = HorarioForm()
 
-    return render(request, "horario/create.html", {"form": form})
+        return super().dispatch(request, *args, **kwargs)
+    
 
+class HorarioDetailView(DetailView):
+    model = Horario
+    template_name = "horario/detail.html"
+    context_object_name = "horario"
+    pk_url_kwarg = "id"
 
-@acesso_required(tipos=["MONITOR", "ADMIN"], permissoes=["users.view_user"])
-def horario_delete(request, id):
-    horario = get_object_or_404(Horario, id=id)
-
-    if request.user != horario.monitor and request.user.tipo != "ADMIN":
-        return redirect("/horarios/")
-
-    horario.delete()
-    return redirect("/horarios/")
-
-
-def horario_detail(request, id):
-    horario = get_object_or_404(Horario, id=id)
-    agendamentos = Agendamento.objects.filter(horario=horario)
-
-    return render(
-        request,
-        "horario/detail.html",
-        {"horario": horario, "agendamentos": agendamentos},
-    )
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["agendamentos"] = Agendamento.objects.filter(horario=self.object)
+        return context
